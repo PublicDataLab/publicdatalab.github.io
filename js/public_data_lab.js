cf29e189fc01f3fa081c8ae2a1960504989a5191
vathsav <vathsav@live.in>
2017-03-31 15:10:25 +0200

    feat: test with threejs

--- js/public_data_lab.js
+++ js/public_data_lab.js
@@ -1,131 +1,117 @@
-var count;
-var chunks;
-var force;
-var windSpeed;
-var maxWind;
-var minWind;
-var particlesAlpha;
+if (!Detector.webgl) Detector.addGetWebGLMessage();
+var container, stats;
+var numberOfParticles = 1500;
+var camera, scene, renderer, controls, particles, geometry, materials = [],
+    parameters, i, h, color, size, group;
+var mouseX = 0,
+    mouseY = 0;
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+var accelerometerX, accelerometerY, accelerometerZ;
 
-var curSize;
-var maxSize;
+init();
 
-function setup() {
-  var canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+function init() {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+    camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 1, 3000);
+    camera.position.z = 1000;
+    controls = new THREE.DeviceOrientationControls(camera);
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xEBFF00);
+    geometry = new THREE.Geometry();
+    group = new THREE.Group();
+    scene.add(group);
 
-  //find the highr value
-  maxSize = windowWidth > windowHeight ? windowWidth : windowHeight;
-  //set current size to max
-  curSize = maxSize;
+    parameters = [
+        [
+            [1, 1, 0.5], 5
+        ],
+        [
+            [0.95, 1, 0.5], 4
+        ],
+        [
+            [0.90, 1, 0.5], 3
+        ],
+        [
+            [0.85, 1, 0.5], 2
+        ],
+        [
+            [0.80, 1, 0.5], 1
+        ]
+    ];
 
-
-  canvas.parent("sketch-holder")
-  count = 350;
-  chunks = [];
-  //horizontal spinning
-  windSpeed = 0.000005;
-  minWind = 0.000005;
-  maxWind = 0.05
-
-  //
-  particlesAlpha = 100;
-  createChunks(count);
-  this.mouseX = mouseX + width / 2;
-  this.mouseY = mouseY + height / 2;
-}
-
-function draw() {
-
-  console.log(touches.length);
-  
-  if(mouseIsPressed || touches.length > 0) {
-
-    if(curSize > 0.1) {
-      curSize --;
-      windSpeed += 0.0000001;
-      particlesAlpha += 0.5
+    for (i = 0; i < numberOfParticles; i++) {
+        var vector = new THREE.Vector3();
+        // Scene dimensions
+        vector.x = Math.random() * 2000 - 1000;
+        vector.y = Math.random() * 2000 - 1000;
+        vector.z = Math.random() * 2000 - 1000;
+        geometry.vertices.push(vector);
     }
 
-  } else {
-
-    if(curSize < maxSize) {
-      curSize += 2;
-      windSpeed -= 0.0000002;
-      particlesAlpha -= 1;
+    for (i = 0; i < parameters.length; i++) {
+        color = parameters[i][0];
+        size = Math.random() * 6 - 1;
+        materials[i] = new THREE.PointsMaterial({
+            size: size
+        });
+        particles = new THREE.Points(geometry, materials[i]);
+        group.add(particles);
     }
-  };
 
-  //define distance scale
-  var distScale = curSize/maxSize;
-
-  handleEnvironment();
-  //for each particle set the position 
-  for (i = 0; i < chunks.length; i++) {
-
-    var chunk = chunks[i];
-    push();
-    //increase rotation according to wind speed
-    chunk.rotation += windSpeed
-    //rotate the axis according the rotation values modified by distance
-    rotateY(chunk.rotation * dist(0, 0, 0, chunk.positionX, chunk.positionY, chunk.positionZ));
-    //move to position
-    translate(chunk.positionX * distScale, chunk.positionY * distScale, chunk.positionZ * distScale);
-    fill(color(55, 0, 60, particlesAlpha));
-    rotateX(frameCount * chunk.rotateX);
-    rotateY(frameCount * chunk.rotateY);
-    rotateZ(frameCount * chunk.rotateZ);
-    plane(chunk.size);
-    pop();
-  }
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+    // stats = new Stats();
+    // container.appendChild(stats.dom);
+    window.addEventListener("devicemotion", handleMotionEvent, true);
+    document.addEventListener('mousemove', onDocumentMouseMove, false);
+    window.addEventListener('resize', onWindowResize, false);
 }
 
-function Chunk() {
-  this.positionX;
-  this.positionY;
-  this.positionZ;
-  this.rotateX;
-  this.rotateY;
-  this.rotateZ;
-  //
-  this.rotation;
-  this.size;
+function animate() {
+    for (i = 0; i < materials.length; i++) {
+        color = parameters[i][0];
+        materials[i].color.setHex(0xdbd82b);
+    }
+
+    group.rotation.y += 0.004;
+    camera.lookAt(scene.position);
+
+    camera.position.x += (mouseX - camera.position.x) * 0.02;
+    camera.position.y += (-mouseY - camera.position.y) * 0.02;
+
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(animate);
+};
+
+function onWindowResize() {
+    windowHalfX = window.innerWidth / 2;
+    windowHalfY = window.innerHeight / 2;
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function createChunks(count) {
-  // create a bunch of particle objects
-  for (i = 0; i < count; i++) {
-    var chunk = new Chunk();
-    chunk.positionX = random(-maxSize / 3, maxSize / 3);
-    chunk.positionY = random(-maxSize / 3, maxSize / 3);
-    chunk.positionZ = random(-maxSize / 3, maxSize / 3);
-    chunk.rotateX = random(0.001, 0.05);
-    chunk.rotateY = random(0.001, 0.05);
-    chunk.rotateZ = random(0.001, 0.05)  * chunk.positionZ * 0.005;
-    chunk.size = random(1, 5);
-    chunk.rotation = 0;
-    chunks[i] = chunk;
-  }
-}
+function handleMotionEvent(event) {
+    accelerometerX = Math.round(event.accelerationIncludingGravity.x * 10);
+    accelerometerY = Math.round(event.accelerationIncludingGravity.y * 10);
 
-function handleEnvironment() {
-  // if (isMousePressed) activateVaccuum();
-  if (accelerationX !=0 || accelerationY !=0 || accelerationZ != 0) {
-    if (deviceOrientation == 'landscape') {
-      this.rotateX(int(rotationY) * 0.004);
-      this.rotateY(int(rotationX) * 0.004);
+    if (window.innerWidth < window.innerHeight) {
+        camera.position.x += (accelerometerX - camera.position.x) * 0.15;
+        camera.position.y += (-accelerometerY - camera.position.y) * 0.15;
     } else {
-      this.rotateX(int(rotationX) * 0.004);
-      this.rotateY(int(rotationY) * 0.004);
+        camera.position.x += (accelerometerY - camera.position.x) * 0.15;
+        camera.position.y += (-accelerometerX - camera.position.y) * 0.15;
     }
-  } else {
-    // var speedX = abs(mouseX - pmouseX);
-    // var speedY = abs(mouseY - pmouseY);
-    this.rotateX(mouseY * -0.0005);
-    this.rotateY(mouseX * -0.0005);
-    // this.rotateX(mouseY * -0.001 + speedY / 1000);
-    // this.rotateY(mouseX * -0.001 + speedX / 1000);
-  }
-
-  normalMaterial();
-  var cameraZ = (height / 2.0) / tan(PI / 3 / 2.0);
-  perspective(PI / 3, width / height, cameraZ / 10.0, cameraZ * 10.0);
 }
+
+function onDocumentMouseMove(event) {
+    mouseX = event.clientX - windowHalfX;
+    mouseY = event.clientY - windowHalfY;
+
+}
+
+animate();
